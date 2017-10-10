4f156475217e56de04e02ae01997cc5050990e85
rifardity <rifardity@gmail.com>
2017-10-10 21:57:50 +0700

    Add class Pasien & Obat

--- app/class_obat.php
+++ app/class_obat.php
@@ -1,39 +1,93 @@
 <?php
+require_once 'database.php';
 class Obat
 {
     private $kode_obat;
     private $nama_obat;
     private $harga_obat;
     private $tipe_obat;
+    private $db;
 
   function __construct(argument)
   {
-
+    $link = new Database();
+    $newdb = $link->get_database();
+    $this->db = $newdb;
   }
 
   public function tambah_obat($kode_obat,$nama_obat,$harga_obat,$tipe_obat){
-    $sql = "INSERT INTO obat(kode_obat,nama_obat,harga_obat,tipe_obat) VALUES('$kode_obat','$nama_obat','$harga_obat','$tipe_obat') ";
-
+    try {
+      $sql = $this->db->prepare("INSERT INTO obat(kode_obat,nama_obat,harga_obat,tipe_obat) VALUES(:kode_obat,:nama_obat,:harga_obat,:tipe_obat)");
+      $sql->bindparam(":kode_obat",$kode_obat);
+      $sql->bindparam(":nama_obat",$nama_obat);
+      $sql->bindparam(":harga_obat",$harga_obat);
+      $sql->bindparam(":tipe_obat",$tipe_obat);
+      $sql->execute();
+      return true;
+    } catch (PDOException $e) {
+      die("Gagal Menyimpan Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function edit_obat($kode_obat){
-    $sql = "SELECT * FROM obat WHERE kode_obat='$kode_obat'";
+    try {
+      $sql = $this->db->prepare("SELECT * FROM obat WHERE kode_obat=:kode_obat");
+      $sql->execute(array(":kode_obat"=>$kode_obat));
+      $data = $sql->fetch(PDO::FETCH_OBJ);
+      return $data;
+    } catch (PDOException $e) {
+      die("Gagal Mengambil Data ".$kode_obat." Error ".$e->getMessage());
+      return false;
+    }
   }
 
   public function simpan_obat($kode_obat,$nama_obat,$harga_obat,$tipe_obat){
-    $sql = "UPDATE obat SET kode_obat='$kode_obat',nama_obat='$nama_obat',harga_obat='$harga_obat',tipe_obat='$tipe_obat'";
+    try {
+      $sql = $this->db->prepare("UPDATE obat SET nama_obat=:nama_obat,harga_obat=:harga_obat,tipe_obat=:tipe_obat WHERE kode_obat=:kode_obat");
+      $sql->bindparam(":nama_obat",$nama_obat);
+      $sql->bindparam(":harga_obat",$harga_obat);
+      $sql->bindparam(":tipe_obat",$tipe_obat);
+      $sql->bindparam(":kode_obat",$kode_obat);
+      $sql->execute();
+      return true;
+    } catch (PDOException $e) {
+      die("Gagal Menyimpan Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function tampil_obat(){
-    $sql = "SELECT * FROM obat";
+    try {
+      $sql = $this->db->prepare("SELECT * FROM obat");
+      return $sql;
+    } catch (PDOException $e) {
+      die("Gagal Mengambil Data Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function hapus_obat($kode_obat){
-    $sql = "DELETE FROM obat WHERE kode_obat='$kode_obat'";
+    try {
+      $sql = $this->db->prepare("DELETE FROM obat WHERE kode_obat=:kode_obat");
+      $sql->bindparam("kode_obat",$kode_obat);
+      $sql->execute();
+      return true;
+    } catch (PDOException $e) {
+      die("Gagal Menghapus Data Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function hitung_obat(){
-    $sql = "SELECT COUNT(*) FROM obat";
+    try {
+      $sql = $this->db->prepare("SELECT COUNT(*) FROM obat");
+      $sql->execute();
+      return $sql;
+    } catch (PDOException $e) {
+      die("Gagal Menghitung Data Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
 }
--- app/class_pasien.php
+++ app/class_pasien.php
@@ -1,40 +1,91 @@
 <?php
+require_once 'database.php';
 class Pasien
 {
-    private $kode_pasien;
-    private $nama_pasien;
-    private $alamat_pasien;
-    private $telepon_pasien;
-    private $penyakit_pasien;
+    private $db;
 
   function __construct(argument)
   {
-
+    $link = new Database();
+    $newdb = $link->get_database();
+    $this->db = $newdb;
   }
 
   public function tambah_pasien($kode_pasien,$nama_pasien,$alamat_pasien,$telepon_pasien,$penyakit_pasien){
-    $sql = "INSERT INTO pasien(kode_pasien,nama_pasien,alamat_pasien,telepon_pasien,penyakit_pasien) VALUES('$kode_pasien','$nama_pasien','$alamat_pasien','$telepon_pasien','$penyakit_pasien') ";
-
+    try {
+      $sql = $this->db->prepare("INSERT INTO pasien(kode_pasien,nama_pasien,alamat_pasien,telepon_pasien,penyakit_pasien) VALUES(:kode_pasien,:nama_pasien,:alamat_pasien,:telepon_pasien,:penyakit_pasien)");
+      $sql->bindparam(":kode_pasien",$kode_pasien);
+      $sql->bindparam(":nama_pasien",$nama_pasien);
+      $sql->bindparam(":alamat_pasien",$alamat_pasien);
+      $sql->bindparam(":telepon_pasien",$telepon_pasien);
+      $sql->bindparam(":penyakit_pasien",$penyakit_pasien);
+      $sql->execute();
+      return true;
+    } catch (PDOException $e) {
+      die("Gagal Menyimpan Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function edit_pasien($kode_pasien){
-    $sql = "SELECT * FROM pasien WHERE kode_pasien='$kode_pasien'";
+    try {
+      $sql = $this->db->prepare("SELECT * FROM pasien WHERE kode_pasien=:kode_pasien");
+      $sql->execute(array(":kode_pasien"=>$kode_pasien));
+      $data = $sql->fetch(PDO::FETCH_OBJ);
+      return $data;
+    } catch (PDOException $e) {
+      die("Gagal Mengambil Data ".$kode_pasien." Error ".$e->getMessage());
+      return false;
+    }
   }
 
   public function simpan_pasien($kode_pasien,$nama_pasien,$alamat_pasien,$telepon_pasien,$penyakit_pasien){
-    $sql = "UPDATE pasien SET kode_pasien='$kode_pasien',nama_pasien='$nama_pasien',alamat_pasien='$alamat_pasien',telepon_pasien='$telepon_pasien',penyakit_pasien='$penyakit_pasien'";
+    try {
+      $sql = $this->db->prepare("UPDATE pasien SET nama_pasien=:nama_pasien,alamat_pasien=:alamat_pasien,telepon_pasien=:telepon_pasien,penyakit_pasien=:penyakit_pasien WHERE kode_pasien=:kode_pasien");
+      $sql->bindparam(":nama_pasien",$nama_pasien);
+      $sql->bindparam(":alamat_pasien",$alamat_pasien);
+      $sql->bindparam(":telepon_pasien",$telepon_pasien);
+      $sql->bindparam(":penyakit_pasien",$penyakit_pasien);
+      $sql->bindparam(":kode_pasien",$kode_pasien);
+      $sql->execute();
+      return true;
+    } catch (PDOException $e) {
+      die("Gagal Menyimpan Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function tampil_pasien(){
-    $sql = "SELECT * FROM pasien";
+    try {
+      $sql = $this->db->prepare("SELECT * FROM pasien");
+      return $sql;
+    } catch (PDOException $e) {
+      die("Gagal Mengambil Data Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function hapus_pasien($kode_pasien){
-    $sql = "DELETE FROM pasien WHERE kode_pasien='$kode_pasien'";
+    try {
+      $sql = $this->db->prepare("DELETE FROM pasien WHERE kode_pasien=:kode_pasien");
+      $sql->bindparam("kode_pasien",$kode_pasien);
+      $sql->execute();
+      return true;
+    } catch (PDOException $e) {
+      die("Gagal Menghapus Data Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
   public function hitung_pasien(){
-    $sql = "SELECT COUNT(*) FROM pasien";
+    try {
+      $sql = $this->db->prepare("SELECT COUNT(*) FROM pasien");
+      $sql->execute();
+      return $sql;
+    } catch (PDOException $e) {
+      die("Gagal Menghitung Data Eror : ".$e->getMessage());
+      return false;
+    }
   }
 
 }
